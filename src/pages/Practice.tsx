c363a05a1a1232ab20941119ab8cd0d545e8559d
Steve Warsa <swarsa480@gmail.com>
2022-01-01 07:13:41 -0700

    Now have the practice setup page working to set the practice config in the store and then route to the actual practice session (WIP)

--- src/pages/Practice.tsx
+++ src/pages/Practice.tsx
@@ -5,7 +5,7 @@ const Practice = () => {
     const practiceConfig = useSelector((state: AppState) => state.practiceConfig);
     console.log("Here is the practice config from the store:");
     console.log(practiceConfig);
-    // TODO - use effect here to grab the memory verses from the store based on the practice config...
+    // TODO - use effect here to grab the memory verses from the server based on the practice config...
     return <h3>Practice Component</h3>
 };
 
