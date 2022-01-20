5a74556df2a305d00b3fbea055a1525998b56947
Steve Warsa <swarsa480@gmail.com>
2022-01-20 14:53:33 -0700

    Make sure that the basename for BrowserRouter object in index.tsx only gets set for production and not for development

--- src/index.tsx
+++ src/index.tsx
@@ -6,8 +6,12 @@ import { Provider } from 'react-redux';
 import App from './App';
 import store from "./store";
 
+console.log("index.tsx - Here are the environment variables: ");
+console.log(process.env);
+const { NODE_ENV } = process.env;
+console.log("index.tsx - Here is the public URL: " + NODE_ENV);
 ReactDOM.render(
-    <BrowserRouter  basename="/bible-app">
+    <BrowserRouter basename={NODE_ENV && NODE_ENV === "development" ? "" : "/bible-app"}>
         <Provider store={store}>
             <App />
         </Provider>
